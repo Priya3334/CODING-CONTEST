3d6fb680bc1dc94e8dd29e4966c354e94b53fe05
Priya3334 <129720939+Priya3334@users.noreply.github.com>
2023-04-05 16:18:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './day2/Header';
+import Product from './day2/Product';
+import Footer from './day2/Footer';
+import Content from './day2/Content';
+import Day1 from './day1';
+import Day4 from './day4';
+import Day3 from './day3';
+import Day5 from './day5';
+
+function App() {
+  return (
+    <div >
+
+      <Day1/>
+      <Day4/>
+      <Day3/>
+      <Day5/>
+
+
+ <Header/>
+ <Product image=""/>
+ <Content/>
+ <Footer/>
+    </div>
+  );
+}
+
+export default App;
